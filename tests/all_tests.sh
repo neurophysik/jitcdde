3ec44d11ccb04f1a6b56e265c61b6e08cef8c0ca
Gerrit Ansmann <bla@example.com>
2017-10-22 15:35:59 +0200

    Removing Python 2 baggage.

--- tests/all_tests.sh
+++ tests/all_tests.sh
@@ -1,8 +1,3 @@
-python -m unittest discover -b -f
 python3 -m unittest discover -b -f
-
-python compare_C_and_Python_core.py 30
 python3 compare_C_and_Python_core.py 30
-
-python compare_C_and_Python_core_2.py 30
 python3 compare_C_and_Python_core_2.py 30
